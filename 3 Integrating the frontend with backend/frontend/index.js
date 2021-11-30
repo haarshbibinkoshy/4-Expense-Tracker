f4c2c8f6326034e6b4bcf4b1749d7ee4284be908
haarsh bibin koshy <haarshbibinkoshy.4127.92@gmail.com>
2021-11-30 11:42:04 +0530

    integrated frontend and backend

---
+++ 3 Integrating the frontend with backend/frontend/index.js
@@ -0,0 +1,18 @@
+function submitForm(e) {
+    e.preventDefault();
+    const form=new FormData(e.target)
+    const signUpDetails={
+        name:form.get('name'),
+        email:form.get('email'),
+        phone:form.get('phone'),
+        password:form.get('password')
+    }
+   
+    axios.post(`http://localhost:3000/signup`,signUpDetails).then(res => {
+        
+        alert(res.data.message);
+    }).catch(err => {
+        console.log(err);
+        alert(`User already exists, Please Login`)
+    })
+}
